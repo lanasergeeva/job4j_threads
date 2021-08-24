53b0bbbe79312ef01f8292ef191b50c48ab58121
PoymaLana <marikakopina@gmail.com>
2021-08-24 17:23:38 +0300

    Подключаю travis и codecov картинки

---
+++ README.md
@@ -0,0 +1,5 @@
+# job4j_threads
+
+[![Build Status](https://app.travis-ci.com/lanasergeeva/job4j_threads.svg?branch=master)](https://app.travis-ci.com/lanasergeeva/job4j_threads)
+
+[![codecov](https://codecov.io/gh/lanasergeeva/job4j_threads/branch/master/graph/badge.svg?token=T19BBKCMMH)](https://codecov.io/gh/lanasergeeva/job4j_threads)
--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,6 +10,6 @@ public class AppTest {
     @Test
     public void whenSum() {
         App ap = new App();
-        assertThat(ap.sum(1,3), is(4));
+        assertThat(ap.sum(1, 3), is(4));
     }
 }
